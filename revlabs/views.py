6258bc4ca59e9ba5f56295b78242ea349136003b
Victor Carraly <vcarraly@gmail.com>
2026-03-30 19:27:27 -0300

    Use dynamic car list and pass car to dashboard
    Replace hardcoded car cards with a loop in car_selection.html and update dashboard.html to render selected car fields (image, name, time). Add a CARS dictionary in views.py, return CARS.values() to the selection template, and implement dashboard view to pick a car from ?car=<id> (defaulting to Mercedes) and pass it to the template.
    
    Co-Authored-By: Arthur Vitorino <arthur.vft@gmail.com>

--- revlabs/views.py
+++ revlabs/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-def dashboard(request):
-    return render(request, 'simulator/dashboard.html')
+CARS = {
+    'mercedes': {'id': 'mercedes', 'name': "Mercedes-AMG GT Black Series '20", 'image': 'img/mercedes-amg.png', 'time': '1:38.175'},
+    'ferrari':  {'id': 'ferrari',  'name': "Ferrari 458 Italia '09",           'image': 'img/ferrari-458.png',  'time': '1:41.200'},
+    'porsche':  {'id': 'porsche',  'name': "Porsche 911 GT3 RS (992) '22",    'image': 'img/porsche-911.png',  'time': '1:39.500'},
+    'parati':   {'id': 'parati',   'name': "VW Parati",                        'image': 'img/vw-parati.png',    'time': '2:15.300'},
+    'fusca':    {'id': 'fusca',    'name': "VW Fusca",                         'image': 'img/vw-fusca.png',     'time': '2:30.000'},
+    'brasilia': {'id': 'brasilia', 'name': "VW Brasilia",                      'image': 'img/vw-brasilia.png',  'time': '2:25.500'}
+}
 
 def car_selection(request):
-    return render(request, 'simulator/car_selection.html')
+    return render(request, 'simulator/car_selection.html', {'cars': CARS.values()})
+
+def dashboard(request):
+    car_id = request.GET.get('car', 'mercedes') 
+    selected_car = CARS.get(car_id, CARS['mercedes'])
+    
+    return render(request, 'simulator/dashboard.html', {'car': selected_car})
